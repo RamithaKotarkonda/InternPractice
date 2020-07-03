85e71536bfb5fbf3afeae4266fecf81e0d0d5d31
RamithaKotarkonda <54600694+RamithaKotarkonda@users.noreply.github.com>
2020-07-03 15:44:16 -0500

    Added lowercase function

--- CommentingPractice/comment-practice.js
+++ CommentingPractice/comment-practice.js
@@ -4,6 +4,7 @@
  */
 import Template from './model/template.js';
 import * as amonte from './services/capitalizer.js';
+import * as ramitha from './services/lowercase.js';
 
 
 
@@ -11,6 +12,8 @@ import * as amonte from './services/capitalizer.js';
     Template.printInstructions();
     var name = "AmonteAndrews";
      console.log(amonte.randomlyCapitalize(name));
+    var word = "WORD";
+    console.log(amonte.lowercase(word));
 
  })()
 
